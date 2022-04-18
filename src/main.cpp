967e742319980aeaae4d54b578c5360b7b91f8d5
PeresMiguel <msouza@realtec.com.br>
2022-04-18 20:57:52 -0300

    3.0

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ const char * password = "miguel997";
 
 
 String FirmwareVer = {
-  "2.9"
+  "3.0"
 };
 #define URL_fw_Version "https://raw.githubusercontent.com/miiguelperes/Placa-01-OTA/main/bin_version.txt"
 #define URL_fw_Bin "https://raw.githubusercontent.com/miiguelperes/Placa-01-OTA/main/fw.bin"
